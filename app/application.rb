fbe9cee5219fc2d270be96bac2b33c6a8de39900
Shea Kusiak <shkusiak@gmail.com>
2018-07-07 16:57:37 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -23,7 +23,12 @@ class Application
         end
       end
     elsif req.path.match(/add/)
-
+      item = req.params["item"]
+      if @@item.include?(item)
+        @@cart << item
+      else
+        resp.write "We don't have that item"
+      end
     else
       resp.write "Path Not Found"
     end
